313638fb3d848dc1eb0adff2ef68fc3dd90166c7
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2021-12-11 15:56:36 -0500

    initial commit

---
+++ assignments/01-values-variables/question1/index.js
@@ -0,0 +1,13 @@
+/*
+Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+*/
+
+let country, continent, population;
+country = "United States";
+continent = "North America";
+population = "not sure";
+
+console.log("Country = ", country);
+console.log("Continent = ", continent);
+console.log("Popluation = ", population);
---
+++ assignments/random-test.js
@@ -0,0 +1,7 @@
+let example = Symbol("45");
+let example2 = Symbol(45);
+
+console.log(example);
+
+example = 23;
+console.log(example);
