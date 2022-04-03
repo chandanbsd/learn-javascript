72ace7c94c2b7f88da6a8bfc1e2ddb99cbdb0a0f
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-04-03 13:07:42 -0400

    added: Data Structures Coding Challenge 3

--- codingChallenges/dataStructuresStringsModernOperators.js
+++ codingChallenges/dataStructuresStringsModernOperators.js
@@ -162,3 +162,55 @@ for (player of game.scored) {
 }
 
 console.log(scorers);
+
+/*
+Let's continue with our football  app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+*/
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+
+events.delete("🔶 Yellow card");
+console.log(events);
+
+const lastEventTime = [...gameEvents.keys()].pop();
+console.log(
+  `An event happened, on average, every ${
+    lastEventTime / gameEvents.size
+  } minutes`
+);
+
+for ([minute, eventName] of gameEvents) {
+  if (minute <= 45) {
+    console.log(`[FIRST HALF] ${minute}:${eventName}`);
+  } else {
+    console.log(`[SECOND HALF] ${minute}:${eventName}`);
+  }
+}
